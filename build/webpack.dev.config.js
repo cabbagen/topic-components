acb5b525e9400ac3e3e575c8bb88983ee335f4e7
cabbagen <754689243@qq.com>
2020-08-30 22:00:11 +0800

    添加 HtmlWebpackPlugin 插件

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,15 +1,29 @@
 
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackBaseConfig = require('./webpack.base.config');
 
 module.exports = Object.assign(webpackBaseConfig, {
     devtool: 'eval-source-map',
 
+    entry: Object.assign({}, webpackBaseConfig.entry, {
+        app: './src/mobile/index.js',
+    }),
+    
     output: {
         path: path.join(__dirname, '../dist/mobile'),
-        filename: 'topic-component.js',
-        library: 'ATopic',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
+        filename: '[name].boundle.js',
     },
+
+    plugins: webpackBaseConfig.plugins.concat([
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'index',
+            filename: 'topic-components.js',
+            minChunks: Infinity,
+        }),
+        new HtmlWebpackPlugin({
+            template: './views/index.html'
+        }),
+    ]),
 });
--- entry/index.js
+++ entry/index.js
@@ -2,10 +2,15 @@ import version from './version';
 import groups from './groups';
 import install, { components } from './install';
 
-
-export default {
+const ATopic = {
     components,
     groups,
     install,
     version,
 };
+
+if (typeof window !== 'undefined') {
+    window.ATopic = ATopic;
+}
+
+export default ATopic;
