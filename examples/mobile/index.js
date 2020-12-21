31af6d084f0141b4e7c1f27c58efe17a18ee2366
liuyixuan <liuyixuan@cnstrong.cn>
2020-12-21 19:35:41 +0800

    add magic component

--- examples/mobile/index.js
+++ examples/mobile/index.js
@@ -3,8 +3,6 @@ if (typeof window.ATopic.default !== 'undefined') {
     Vue.use(window.ATopic.default);
 }
 
-console.log(window.ATopic.default);
-
 var app = new Vue({
     el: '#root',
     data: {
